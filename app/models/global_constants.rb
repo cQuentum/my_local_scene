52ec48d66928ac1de694b85bd4e0632cd6567e8c
Jerome Luce <jerome.luce@protonmail.com>
2020-04-14 18:43:54 +0200

    Add pretty good seeds & personnal accounts for each dev

---
+++ app/models/global_constants.rb
@@ -0,0 +1,12 @@
+module GlobalConstants
+  GENRES = ["Rock", "Metal", "Rap", "RnB", "Classique", "Jazz", "Hip-Hop", "Country", "Blues", "Electronique", "Folk", "Pop",
+"Funk", "Soul", "Reggae", "Ambient", "Ska", "Contemporaine", "Dance"]
+  GENRES_BREST = GENRES
+  GENRES_BREST.delete("Metal")
+  GENRES_QUIMPER = GENRES
+  GENRES_QUIMPER.delete("Rap")
+  GENRES_CARHAIX = GENRES
+  GENRES_CARHAIX.delete("Rock")
+  FAMILY_NAMES = ["Martin", "Bernard", "Thomas", "Petit", "Robert", "Durand", "Dubois", "Moreau", "Lefebvre", "Leroy", "Morel", "Fournier", "Girard", "Bonnet", "Fontaine", "Le Cam", "Le Bail", "Drezen", "Lecoq"]
+  FIRST_NAMES = ["Jean", "Pierre", "Michel", "André", "Philippe", "René", "Louis", "Alain", "Jacques", "Bernard", "Marcel", "Daniel", "Roger", "Robert", "Paul", "Claude", "Christian", "Henri", "Georges", "Nicolas", "François", "Patrick"]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_14_121615) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_121615) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
+
   create_table "bands", force: :cascade do |t|
     t.string "name"
     t.string "genre"
