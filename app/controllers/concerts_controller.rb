75f41ff9737eee3c52958202b790bc0f8d4d835d
Adrien Abadou <adrienabadou@gmail.com>
2020-04-15 12:46:53 +0200

    creating cards

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,6 +2,7 @@ class ConcertsController < ApplicationController
   before_action :set_concert, only: [:show]
 
   def index
+    @concerts = Concert.all.reverse
   end
 
   def show
@@ -17,7 +18,7 @@ class ConcertsController < ApplicationController
   private
 
   def concert_params
-    params.require(:concert).permit(:title, :address, :description, :external_link, :price_cents, :confirmed, :start_time, :end_time, :latitude, :longitude)
+    params.require(:concert).permit(:title, :address, :description, :external_link, :price_cents, :confirmed, :start_time, :end_time, :latitude, :longitude, :photo)
   end
 
   def set_concert
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @concerts = Concert.all.reverse
+  end
+
+  private
+
+  def concert_params
+    params.require(:concert).permit(:title, :address, :description, :external_link, :price_cents, :confirmed, :start_time, :end_time, :latitude, :longitude, :photo)
   end
 end
