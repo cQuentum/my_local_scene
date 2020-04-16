5f193a62eb3c07f299fa0a82cd7ec2fe9dc45f21
Jerome Luce <jerome.luce@protonmail.com>
2020-04-16 08:45:08 +0200

    Fix flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,20 @@
 import flatpickr from "flatpickr";
 import { French } from "flatpickr/dist/l10n/fr.js"
+
 flatpickr(".datepicker", {
   enableTime: true,
   altInput: true,
   "locale": French,
   altFormat: "H:i D d M Y",
 });
+
+const initFlatpickr = () => {
+  flatpickr(".datepicker", {
+  enableTime: true,
+  altInput: true,
+  "locale": French,
+  altFormat: "H:i D d M Y",
+});
+}
+
+export { initFlatpickr }
