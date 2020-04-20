18bbeb7f0bfa6b1ecb60377db448096998221a12
Adrien Abadou <adrienabadou@gmail.com>
2020-04-20 17:49:32 +0200

    A user can now have no genres selected

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,11 +15,13 @@ class ProfilesController < ApplicationController
   def update
     user_to_update = User.find(params[:id])
     params[:user][:genres]
+    user_to_update.genres = [] if params[:user][:genres].blank?
     user_to_update.update(update_params)
     redirect_to profile_path(user_to_update)
   end
 
 private
+
   def update_params
     params.require(:user).permit(:location, :move_radius, genres: [])
   end
