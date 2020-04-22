553dd5ac463e6f31f11519c4b8beb1bac0768915
Quentin Messager <qmessager@gmail.com>
2020-04-22 13:27:12 +0200

    added the possibility to cancel participation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :concerts, only: [:index, :show] do
-    resources :participations, only: [:create]
+    resources :participations, only: [:create, :destroy]
   end
 
   resources :participations, only: [:index]
