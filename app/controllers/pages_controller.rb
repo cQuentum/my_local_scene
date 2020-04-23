d2a238549d8318eef3db5e3688cc81d28b3494f5
Adrien Abadou <adrienabadou@gmail.com>
2020-04-23 14:40:13 +0200

    Displaying the first 3 genres the user selected in the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @user = current_user
     if user_signed_in? && current_user.genres && current_user.location && current_user.move_radius
       geo_accurate_concerts = Concert.near([current_user.latitude, current_user.longitude], current_user.move_radius)
       @concerts = geo_accurate_concerts.joins(:band).where(bands: {genre: current_user.genres} ).sort_by &:start_time
